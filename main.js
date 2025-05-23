bd71a8359321c8772c3e2604f33f528bc0c87281
F-Shouter <yukari.yamasaki1809@gmail.com>
2025-05-23 14:55:48 -0300

    mudanças atualizadas

--- main.js
+++ main.js
@@ -18,11 +18,11 @@ const serial = async (
     // conexão com o banco de dados MySQL
     let poolBancoDados = mysql.createPool(
         {
-            host: '10.18.32.129',
-            user: 'aluno',
-            password: 'Sptech#2024',
+            host: '10.18.33.10',
+            user: 'root',
+            password: 'Urubu@100',
             database: 'sgc',
-            port: 3307
+            port: 3306
         }
     ).promise();
 
